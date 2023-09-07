d896c5f27d91a1d6ba35ac28e4418fc57e7d637a
Swarnim B <swarnim14.9@hotmail.com>
2023-09-07 22:24:00 +0530

    updated bin search

--- 001-binary-search/main.py
+++ 001-binary-search/main.py
@@ -1,5 +1,5 @@
 """_summary_
-QUESTION 1: Alice has some cards with numbers written on them.
+QUESTION: Alice has some cards with numbers written on them.
 She arranges the cards in decreasing order, and lays them out face down in a sequence on a table.
 She challenges Bob to pick out the card containing a given number by turning over as few cards as possible.
 Write a function to help Bob locate the card.
